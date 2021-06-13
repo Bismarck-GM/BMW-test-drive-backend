ac7de342b3ce03fb453199cdf45fb27cb832e027
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-13 16:01:23 -0300

    Edit Appointment Model. Fix rubocop issues. Fix logic in private methods.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -21,49 +21,45 @@ class Appointment < ApplicationRecord
   validate :after_two_months?
 
   private
-  
+
   def manipulate_date
-    self.start_time = self.start_time.to_datetime
-    self.end_time = self.start_time + 30.minutes
+    self.start_time = start_time.to_datetime
+    self.end_time = start_time + 30.minutes
   end
 
   def car_exists?
-    if Car.find_by_id(car_id).nil?
-      errors.add(:error, "You must select a valid car for the appointment.")
-      return
-    end
+    return unless Car.find_by_id(car_id).nil?
+
+    errors.add(:error, 'You must select a valid car for the appointment.')
   end
-  
+
   def dealership_exist?
-    if Dealership.find_by_id(dealership_id).nil?
-      errors.add(:error, "You must select a valid dealership for the appointment.")
-      return
-    end
+    return unless Dealership.find_by_id(dealership_id).nil?
+
+    errors.add(:error, 'You must select a valid dealership for the appointment.')
   end
 
   def after_today?
-    if start_time < DateTime.tomorrow && start_time.day == DateTime.now.day 
-      errors.add(:error, "Sorry. You can't set an appointment in the same day.")
-      return
-    end
+    return unless !start_time.nil? && start_time < DateTime.tomorrow && start_time.day == DateTime.now.day
+
+    errors.add(:error, "Sorry. You can't set an appointment in the same day.")
   end
 
   def o_clock_time?
-    if start_time.min != 0 && start_time.min != 30
-      errors.add(:error, "Sorry. The appointment can only be setted in 30 min partials. Ie: '15:00' or '15:30'.")
-    end
+    return unless !start_time.nil? && start_time.min != 0 && start_time.min != 30
+
+    errors.add(:error, "Sorry. The appointment can only be setted in 30 min partials. Ie: '15:00' or '15:30'.")
   end
 
   def not_in_the_past?
-    if start_time < DateTime.now
-      errors.add(:error, "The appointment can't be set in the past.")
-    end
+    return unless !start_time.nil? && start_time < DateTime.now
+
+    errors.add(:error, "The appointment can't be set in the past.")
   end
 
   def after_two_months?
-    if start_time > DateTime.now + 2.months
-      errors.add(:error, "Sorry. The appointment can't be set past 2 months in the future.")
-    end
-  end
+    return unless !start_time.nil? && start_time > DateTime.now + 2.months
 
+    errors.add(:error, "Sorry. The appointment can't be set past 2 months in the future.")
+  end
 end
